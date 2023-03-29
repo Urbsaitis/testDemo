7e70cfae022cfd6ed364dda43ccfe4b3738c1c18
turbs <t.urbsaitis@gmail.com>
2023-03-30 02:23:57 +0300

    Add TestSeleniumDemo

---
+++ src/test/java/lt/tomas/demo/TestSeleniumDemo.java
@@ -0,0 +1,67 @@
+package lt.tomas.demo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestSeleniumDemo {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("start-maximized");
+        options.addArguments("--force-device-scale-factor=0.7");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+        driver.get("https://demoqa.com/text-box");
+    }
+
+    @Test
+    public void seleniumDemo() {
+
+    }
+
+    @Test
+    public void testFullNameInput_FullNameValueDailius() {
+
+        String messageFullName = "Tomas Urbsaitis";
+        String expectedResult = "Tomas Urbsaitis";
+        String actualResult;
+
+        WebElement inputUserName = driver.findElement(By.xpath("//input[@id='userName']"));
+        inputUserName.sendKeys(messageFullName);
+
+        WebElement buttonSubmit = driver.findElement(By.xpath("//button[@id='submit']"));
+        buttonSubmit.click();
+
+        WebElement paragraphName = driver.findElement(By.xpath("//p[@id='name']"));
+        actualResult = paragraphName.getText(); // Name:Dailius Rascius
+
+        Assert.assertTrue(
+                actualResult.contains(expectedResult),
+                String.format("Actual: %s; Expected: %s", actualResult, expectedResult)
+        );
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
