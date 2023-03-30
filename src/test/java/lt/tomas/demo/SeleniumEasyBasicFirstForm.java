fac9b327978fd38ad9035ec824ede410f31779fb
turbs <t.urbsaitis@gmail.com>
2023-03-30 21:18:19 +0300

    Add SeleniumEasyBasicFirstForm two input fieds

--- src/test/java/lt/tomas/demo/SeleniumEasyBasicFirstForm.java
+++ src/test/java/lt/tomas/demo/SeleniumEasyBasicFirstForm.java
@@ -58,10 +58,60 @@ public class SeleniumEasyBasicFirstForm {
 
     }
 
+    @Test
+    public void twoInputFieldsValid() {
+        String enterA = "10";
+        String enterB = "11";
+        String expectedResult = "21";
+        String actualResult;
+
+        WebElement inputA = driver.findElement(By.xpath("//input[@id='sum1']"));
+        inputA.sendKeys(enterA);
+
+        WebElement inputB = driver.findElement(By.xpath("//input[@id='sum2']"));
+        inputB.sendKeys(enterB);
+
+        WebElement buttonGetTotal = driver.findElement(By.xpath
+                ("//button[@onclick='return total()']"));
+        buttonGetTotal.click();
+
+        WebElement totalSum = driver.findElement(By.xpath("//span[@id='displayvalue']"));
+        actualResult = totalSum.getText();
+
+        Assert.assertTrue(
+                actualResult.contains(expectedResult),
+                String.format("Actual: %s; Expected: %s", actualResult, expectedResult)
+        );
+
+    }
 
 
+    @Test
+    public void twoInputFieldsNotValid() {
+        String enterA = "bb";
+        String enterB = "11";
+        String expectedResult = "NaN";
+        String actualResult;
 
+        WebElement inputA = driver.findElement(By.xpath("//input[@id='sum1']"));
+        inputA.sendKeys(enterA);
 
+        WebElement inputB = driver.findElement(By.xpath("//input[@id='sum2']"));
+        inputB.sendKeys(enterB);
+
+        WebElement buttonGetTotal = driver.findElement(By.xpath
+                ("//button[@onclick='return total()']"));
+        buttonGetTotal.click();
+
+        WebElement totalSum = driver.findElement(By.xpath("//span[@id='displayvalue']"));
+        actualResult = totalSum.getText();
+
+        Assert.assertTrue(
+                actualResult.contains(expectedResult),
+                String.format("Actual: %s; Expected: %s", actualResult, expectedResult)
+        );
+
+    }
 
 
     @AfterMethod
