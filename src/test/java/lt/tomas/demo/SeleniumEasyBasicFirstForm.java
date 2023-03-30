28c00ab75eaa49bc7f02e2cc676b9b1f92a0f084
turbs <t.urbsaitis@gmail.com>
2023-03-30 20:55:32 +0300

    Add SeleniumEasyBasicFirstForm

---
+++ src/test/java/lt/tomas/demo/SeleniumEasyBasicFirstForm.java
@@ -0,0 +1,73 @@
+package lt.tomas.demo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SeleniumEasyBasicFirstForm {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("start-maximized");
+        options.addArguments("--force-device-scale-factor=0.7");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+        driver.get("http://demo.seleniumeasy.com/basic-first-form-demo.html");
+    }
+
+
+    @Test
+    public void singleInputField() {
+
+        String message = "Labas vakaras";
+        String expectedResult = "Labas vakaras";
+        String actualResult;
+
+        WebElement inputmessage = driver.findElement(By.xpath("//input[@id='user-message']"));
+        inputmessage.sendKeys(message);
+
+        WebElement buttonSubmit = driver.findElement(By.xpath
+                ("//button[@type='button' and @class='btn btn-default' and @onclick='showInput();']"));
+        buttonSubmit.click();
+
+        WebElement yourMessage = driver.findElement(By.xpath("//span[@id='display']"));
+        actualResult = yourMessage.getText();
+
+        Assert.assertTrue(
+                actualResult.contains(expectedResult),
+                String.format("Actual: %s; Expected: %s", actualResult, expectedResult)
+        );
+
+    }
+
+
+
+
+
+
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+}
